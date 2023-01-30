8c8524353b02855f45055bed2e4cf7ccefec0445
shashi <shashi09568@gmail.com>
2023-01-30 15:56:56 +0530

    themeup hero section

--- components/heroSlider/HeroSlider.tsx
+++ components/heroSlider/HeroSlider.tsx
@@ -5,15 +5,20 @@ export default function HeroSlider() {
     return (
         <div className={styles.heroSlider}>
             <div className={`container`}>
-                <div className={`row`}>
+                <div className={`row justify-content-center align-items-center`}>
                     <div className={`col-6`}>
                          <div className={styles.heroSliderMain}>
                             <div className={styles.heroContent}>
-                                <h1 className={`primary`}>The wide range of amenities brand </h1>
-                                <p>Discover our totally innovative products for your needs</p>
+                                <h1 className={`text-secondary`}>The wide range of amenities brand </h1>
+                                <p className={`text-secondary`}>Discover our totally innovative products for your needs</p>
                                 <button className={`btn btn-primary`}>Read More</button>
                             </div>
                          </div>
+                    </div>
+                    <div className={`col-6`}>
+                        <div className={styles.heroBanner}>
+                            <img src={`../images/banner.png`} width={`100%`} />
+                        </div>
                     </div>
                 </div>
                 <div className={styles.heroSliderSection}>

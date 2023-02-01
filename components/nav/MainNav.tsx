6082fbe73e0e8b1619c9dfe2553193ebca68a112
shashi <shashi09568@gmail.com>
2023-02-01 17:29:24 +0530

    themeup hero section

--- components/nav/MainNav.tsx
+++ components/nav/MainNav.tsx
@@ -19,6 +19,7 @@ export default function MainNav() {
     },[])
     return (
         <div className={styles.mainNav +' '+ scroll} >
+
             <div className="container-fluid">
                 <header className="d-flex flex-wrap justify-content-between align-items-center py-0 py-sm-4">
                     <Link href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
@@ -40,6 +41,7 @@ export default function MainNav() {
                     </div>
                 </header>
             </div>
+
         </div>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,15 +10,11 @@ import ServiceSection from "../components/serviceSection/ServiceSection";
 import BrandSection from "../components/brandSection/BrandSection";
 import InsightSection from "../components/insightSection/InsightSection";
 import FooterSection from "../components/footerSection/footerSection";
-import ComingSoon from "../components/comingSoon/comingSoon";
 
 
 export default function Home() {
   return (
     <div>
-        <Head>
-            <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600&display=swap" rel="stylesheet" />
-        </Head>
         <MainNav/>
         <HeroSlider/>
         <AboutSection/>

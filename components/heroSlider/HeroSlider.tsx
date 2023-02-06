8b2c1443e1d4dc375247688e0c117d605a902cce
Janagiramanm <janagiraman@netiapps.com>
2023-02-06 18:36:16 +0530

    hero slider code changes

--- components/heroSlider/HeroSlider.tsx
+++ components/heroSlider/HeroSlider.tsx
@@ -22,72 +22,30 @@ export default function HeroSlider({heroSlider, heroProduct}:any) {
             <div className={styles.heroSlider}>
                 <div>
                     <Slider {...settings}>
-                        <div>
-                            <div className={styles.slide}>
-                                <div className={`container`}>
-                                    <div className={`row justify-content-center align-items-center`}>
-                                        <div className={`col-6`}>
-                                            <div className={styles.heroSliderMain}>
-                                                <div className={styles.heroContent}>
-                                                    <h1 className={`text-secondary`}>{heroSlider[0].title}</h1>
-                                                    <p className={`text-secondary`}>{heroSlider[0].description}</p>
-                                                    <button className={`btn btn-primary`}>{heroSlider[0].link_title}</button>
+                        {heroSlider?.map((element:any, index:any)=>(
+                            <div key={index}>
+                                <div className={styles.slide}>
+                                    <div className={`container`}>
+                                        <div className={`row justify-content-center align-items-center`}>
+                                            <div className={`col-6`}>
+                                                <div className={styles.heroSliderMain}>
+                                                    <div className={styles.heroContent}>
+                                                        <h1 className={`text-secondary`}>{element.title}</h1>
+                                                        <p className={`text-secondary`}>{element.description}</p>
+                                                        <button className={`btn btn-primary`}>{element.link_title}</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className={`col-6`}>
+                                                <div className={styles.heroBanner}>
+                                                    <img src={element.banner_image.url} width={`100%`}  alt={`image`}/>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className={`col-6`}>
-                                            <div className={styles.heroBanner}>
-                                                <img src={heroSlider[0].banner_image.url} width={`100%`}  alt={`image`}/>
-                                            </div>
                                         </div>
-                                    </div>
-                                     </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className={styles.slide}>
-                                <div className={`container`}>
-                                    <div className={`row justify-content-center align-items-center`}>
-                                        <div className={`col-6`}>
-                                            <div className={styles.heroSliderMain}>
-                                                <div className={styles.heroContent}>
-                                                    <h1 className={`text-secondary`}>{heroSlider[0].title}</h1>
-                                                    <p className={`text-secondary`}>{heroSlider[0].description}</p>
-                                                    <button className={`btn btn-primary`}>{heroSlider[0].link_title}</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className={`col-6`}>
-                                            <div className={styles.heroBanner}>
-                                                <img src={heroSlider[0].banner_image.url} width={`100%`}  alt={`image`}/>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div>
-                            <div className={styles.slide}>
-                                <div className={`container`}>
-                                    <div className={`row justify-content-center align-items-center`}>
-                                        <div className={`col-6`}>
-                                            <div className={styles.heroSliderMain}>
-                                                <div className={styles.heroContent}>
-                                                    <h1 className={`text-secondary`}>{heroSlider[0].title}</h1>
-                                                    <p className={`text-secondary`}>{heroSlider[0].description}</p>
-                                                    <button className={`btn btn-primary`}>{heroSlider[0].link_title}</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className={`col-6`}>
-                                            <div className={styles.heroBanner}>
-                                                <img src={heroSlider[0].banner_image.url} width={`100%`}  alt={`image`}/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        ))}
                     </Slider>
 
 

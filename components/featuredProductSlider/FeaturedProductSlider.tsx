bb6995ee7dbcb90e8e163db5d767f2302a6b4e25
shashi <shashi09568@gmail.com>
2023-02-06 18:33:04 +0530

    themeup hero section

--- components/featuredProductSlider/FeaturedProductSlider.tsx
+++ components/featuredProductSlider/FeaturedProductSlider.tsx
@@ -1,9 +1,18 @@
 import next from "next";
+import Slider from "react-slick";
 import styles from "./FeaturedProductSlider.module.scss";
 import React, {} from 'react'
 import Link from "next/link";
 import Image from "next/image";
 export default function FeaturedProductSlider({productSlider}:any) {
+    const settings = {
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "10px",
+        slidesToShow: 3,
+        speed: 500
+    };
     return (
        <div className={styles.featuredSlider}>
            <div className={`container`}>
@@ -11,15 +20,20 @@ export default function FeaturedProductSlider({productSlider}:any) {
                    <h3>{productSlider.title}</h3>
                </div>
            </div>
-           <div className={`container`}>
+
                <div className={styles.SliderSection}>
+                   <Slider {...settings}>
+
                    {productSlider?.slider_product?.map((element:any, index:any)=>(
                         <div className={``} key={index}>
-                            <img alt={`image`} src={element.image.url} />
+                            <div className={`w-75`}>
+                                <img alt={`image`} src={element.image.url} />
+                            </div>
                         </div>
                    ))}
+                   </Slider>
                   
-               </div>
+
            </div>
        </div>
     );

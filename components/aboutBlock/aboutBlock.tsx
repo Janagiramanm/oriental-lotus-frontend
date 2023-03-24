d1eedf3002a9010d9f82963acbd5ee3ca0b87232
shashi <shashi09568@gmail.com>
2023-03-24 17:56:39 +0530

    themeup hero section

--- components/aboutBlock/aboutBlock.tsx
+++ components/aboutBlock/aboutBlock.tsx
@@ -12,7 +12,7 @@ export default function AboutBlock() {
                       <div className={`col-md-12`}>
                           <div className={styles.aboutContent}>
                               <h4>About</h4>
-                              <p>Lotus is a leading direct manufacturer of premium hotel amenities founded in 1997. From the start, our company's motto and goal has been six small words: "Quality and Service You Can Trust." We pride ourselves in supplying amenities that deliver on quality, luxury, and sophistication.</p>
+                              <p>Lotus is a leading direct manufacturer of premium hotel amenities founded in 1997. From the start, our companys motto and goal has been six small words: Quality and Service You Can Trust. We pride ourselves in supplying amenities that deliver on quality, luxury, and sophistication.</p>
                           </div>
                       </div>
                   </div>
@@ -20,19 +20,19 @@ export default function AboutBlock() {
                       <div className={`col-md-6`}>
                           <div className={styles.aboutThree}>
                               <h3>Design</h3>
-                              <p>Today the opportunities are greater than ever. Places of accommodation all around the world are depending on brand loyalty and recognition and we have the expertise to act as your very own concierge of amenities. Our in-house design team allows us to capture a hotel’s individuality and reflect that essence into unique customized guest room amenities. In addition to having our own design team and manufacturing capabilities we have also pre-selected the best vendors in the business, making us your one-direct source for all guest amenities and guest room items.</p>
+                              <p>Today the opportunities are greater than ever. Places of accommodation all around the world are depending on brand loyalty and recognition and we have the expertise to act as your very own concierge of amenities. Our in-house design team allows us to capture a hotel individuality and reflect that essence into unique customized guest room amenities. In addition to having our own design team and manufacturing capabilities we have also pre-selected the best vendors in the business, making us your one-direct source for all guest amenities and guest room items.</p>
                           </div>
                       </div>
                       <div className={`col-md-6`}>
                           <div className={styles.aboutThree}>
                               <h3>Design</h3>
-                              <p>Today the opportunities are greater than ever. Places of accommodation all around the world are depending on brand loyalty and recognition and we have the expertise to act as your very own concierge of amenities. Our in-house design team allows us to capture a hotel’s individuality and reflect that essence into unique customized guest room amenities. In addition to having our own design team and manufacturing capabilities we have also pre-selected the best vendors in the business, making us your one-direct source for all guest amenities and guest room items.</p>
+                              <p>Today the opportunities are greater than ever. Places of accommodation all around the world are depending on brand loyalty and recognition and we have the expertise to act as your very own concierge of amenities. Our in-house design team allows us to capture a hotel individuality and reflect that essence into unique customized guest room amenities. In addition to having our own design team and manufacturing capabilities we have also pre-selected the best vendors in the business, making us your one-direct source for all guest amenities and guest room items.</p>
                           </div>
                       </div>
                   </div>
                   <div className={`row pt-4 pb-4`}>
                       <div className={`col-12 text-center`}>
-                          <h2>Lotus is a leading direct manufacturer of premium hotel amenities founded in 1997. From the start, our company's motto and goal has been six small words: "Quality and Service You Can Trust." We pride ourselves in supplying amenities that deliver on quality, luxury, and sophistication.</h2>
+                          <h2>Lotus is a leading direct manufacturer of premium hotel amenities founded in 1997. From the start, our company's motto and goal has been six small words: Quality and Service You Can Trust. We pride ourselves in supplying amenities that deliver on quality, luxury, and sophistication.</h2>
                       </div>
                   </div>
               </div>

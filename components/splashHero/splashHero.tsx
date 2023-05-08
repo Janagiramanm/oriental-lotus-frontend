aaf2b5a28872c290d2f8d12f231153dee65d73a3
shashi <shashi09568@gmail.com>
2023-05-08 16:47:48 +0530

    themeup hero section

--- components/splashHero/splashHero.tsx
+++ components/splashHero/splashHero.tsx
@@ -1,42 +1,61 @@
 import next from "next";
 import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
+import { Power2 } from 'gsap/all';
 import { CSSPlugin } from "gsap/CSSPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import styles from "./splashHero.module.scss";
-import FooterSection from "../footerSection/footerSection";
-
+import getDocumentElement from "@popperjs/core/lib/dom-utils/getDocumentElement";
 
 
 
 export default function SplashHero({service}:any) {
-    gsap.registerPlugin(CSSPlugin, ScrollTrigger);
 
+    useEffect(()=>{
+       animateBox();
+       animateLogo();
+    },[])
 
     const animateBox = () => {
-        gsap.to(".box", {
-            x: 100,
-            duration: 1,
-            ease: "power2.out",
+        gsap.fromTo(".slogan12",{
+            opacity: 1, y: -100, duration:1, scale: ("1"),
+            },
+        {
+            opacity: 1, delay: 3, y: 60, duration: 1, scale: (".5"), ease: "linear"
         });
+    };
+
+
+
+
+    const animateLogo = () => {
+        gsap.fromTo(".logoAni",{
+                opacity: 0, y: 90, duration:1,
+            },
+            {
+                opacity: 1, delay: 4, duration: 1, y:80, stagger: 1, ease: "bounce",
+            });
     };
 
 
     return (
         <div>
-            <div className="box" onClick={animateBox}>
-                Click me to animate!
-            </div>
             <div className={styles.IntroText}>
-                <div className={`container animate__animated animate__pulse`}>
-                        <div className={styles.logoBlock}>
-                            <div className={styles.icon}><img className={`animate__animated animate__fadeIn animate__delay-2s `} src={"./images/icon.svg"} width={'140px'} /></div>
-                            <div className={styles.icon}><img className={`animate__animated animate__fadeIn animate__delay-3s mb-3 `} src={"./images/logo.svg"}   width={'440px'}  /></div>
-                            <div className={styles.icon}><img className={`animate__animated animate__fadeIn animate__delay-4s `} src={"./images/tagline.svg"}  width={'440px'} /></div>
+
+                <div className={`container`}>
+                        <div className={`${styles.logoBlock} logoAni`}>
+                            <div className={`${styles.icon} logoAni`}><img src={"./images/icon.svg"} width={'140px'} /></div>
+                            <div className={`${styles.icon} logoAni`}><img src={"./images/logo.svg"}   width={'440px'}  /></div>
+                            <div className={`${styles.icon} mt-4 logoAni`}><img src={"./images/tagline.svg"}  width={'440px'} /></div>
                         </div>
+
+                    <div className={``}>
+                        <div className={`slogan12`}><img className={`slogan12`} src={`./images/slogan.svg`} width={`100%`}  /> </div>
+                    </div>
                 </div>
             </div>
+
         </div>
 
     );

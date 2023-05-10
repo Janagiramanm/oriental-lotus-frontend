4e8c2f803155ea61ca21f2163d549fa16b4179a5
shashi <shashi09568@gmail.com>
2023-05-10 11:51:47 +0530

    themeup hero section

--- components/nav/MainNav.tsx
+++ components/nav/MainNav.tsx
@@ -7,8 +7,7 @@ import { ApiService } from "../../services/api.service";
 import axios from "axios";
 export default function MainNav({categories, products, brands, cartItems, menu}: any) {
   
-    console.log('MENUA==',menu[0].acf)
-   
+
     const [scroll, setScroll] = useState('')
     const [dblock, setDblock] = useState(" d-none");
     const [bmenu, setBrandblock] = useState(" d-none");
@@ -48,19 +47,18 @@ export default function MainNav({categories, products, brands, cartItems, menu}:
                         <img width={`120`} src={"/images/logo.png"}  alt={`image`} />
                     </Link>
                     <ul className="d-none d-md-flex nav col-6 col-md-auto mb-2 justify-content-center mb-md-0">
-                         {menu[0].acf.main_menu?.map((element:any, index:any)=>(
+                         {menu[0]?.acf.main_menu?.map((element:any, index:any)=>(
                               <li key={index}><Link href='' onMouseOver={showMenu} onClick={hideMenu}>{element.main_menu_name}</Link>
                                 <div className={ "menuDrop product-menu "+dblock } onMouseLeave={hideMenu}>
                                         <div className={styles.menuWhite}>
                                             <div className={`container`}>
                                                 <div className={`row`}>
                                                     <div className={`col-md-3`}>
-                                                            <ul>
-                                                                {
+                                                            <ul> {
                                                                 element.sub_menu&&element.sub_menu?.map((elem:any, ind:any)=>(
-                                                                    <li key={ind}><Link href='' onClick={hideMenu}>{elem.sub_menu_title}</Link></li>
+                                                                    <li key={ind}> <Link href='/'>{elem.sub_menu_title}</Link> </li>
                                                                 ))
-                                                            }
+                                                                }
                                                             </ul>
 
                                                     </div>

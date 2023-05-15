f2d932fbc3584a814340ea7df6aa2058fdd0c789
shashi <shashi09568@gmail.com>
2023-05-15 13:19:05 +0530

    changes

--- components/aboutBlock/aboutBlock.tsx
+++ components/aboutBlock/aboutBlock.tsx
@@ -20,12 +20,12 @@ export default function AboutBlock() {
                   <div className={`row pt-4 pb-4`}>
                       <div className={`col-md-6`}>
                           <div className={styles.aboutThree}>
-                              <h3>With a total 57,000 sq ft of manufacturing area seamlessly operated by a dedicated workforce of 150 people, we offer one of the largest production capacities in India.</h3>
+                              <p>With a total 57,000 sq ft of manufacturing area seamlessly operated by a dedicated workforce of 150 people, we offer one of the largest production capacities in India.</p>
                           </div>
                       </div>
                       <div className={`col-md-6`}>
                           <div className={styles.aboutThree}>
-                              <h3>Our full-fledged dedicated in-house laboratory develops custom formulations for private labels and produces third-party licensed hotel toiletries either in partnership or through the contract manufacturing route for luxury and couture International Amenities Brands.</h3>
+                              <p>Our full-fledged dedicated in-house laboratory develops custom formulations for private labels and produces third-party licensed hotel toiletries either in partnership or through the contract manufacturing route for luxury and couture International Amenities Brands.</p>
                           </div>
                       </div>
                   </div>

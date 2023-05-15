b0f7f68f2e7d7af0ac67f1517a28643c4c81d5e3
shashi <shashi09568@gmail.com>
2023-05-15 18:58:12 +0530

    changes

--- components/footerSection/footerSection.tsx
+++ components/footerSection/footerSection.tsx
@@ -14,11 +14,11 @@ export default function FooterSection() {
                           <img src={'/images/logo-footer.png'} alt={`image`} width={`100px`} className={`mb-3`}  />
                           <p className={``}>Oriental Lotus is the industry benchmark for manufacturing and production of world class hotel amenities, catering the largest client base of 1500+ premium 5-star and luxury boutique hotels in India</p>
                           <ul className={`d-flex flex-row mb-3 justify-content-center align-items-center`}>
-                              <li className={`px-2`}><a href={`https://www.facebook.com/OrientalLotusHospitalitySolutions`} target={"_blank"}><i className="bi bi-facebook"></i></a></li>
-                              <li className={`px-2`}><a href={`https://www.linkedin.com/company/oriental-lotus-hospitality-solutions`} target={"_blank"}><i className="bi bi-linkedin"></i></a></li>
-                              <li className={`px-2`}><a href={`https://www.instagram.com/olhsindia/`} target={"_blank"}><i className="bi bi-instagram"></i></a></li>
-                              <li className={`px-2`}><a href={`https://twitter.com/OLHSIndia`} target={"_blank"}><i className="bi bi-twitter"></i></a></li>
-                              <li className={`px-2`}><a href={`https://www.youtube.com/@OLHS`} target={"_blank"}><i className="bi bi-youtube"></i></a></li>
+                              <li className={`px-2`}><Link href={`https://www.facebook.com/OrientalLotusHospitalitySolutions`} target={"_blank"}><i className="bi bi-facebook"></i></Link></li>
+                              <li className={`px-2`}><Link href={`https://www.linkedin.com/company/oriental-lotus-hospitality-solutions`} target={"_blank"}><i className="bi bi-linkedin"></i></Link></li>
+                              <li className={`px-2`}><Link href={`https://www.instagram.com/olhsindia/`} target={"_blank"}><i className="bi bi-instagram"></i></Link></li>
+                              <li className={`px-2`}><Link href={`https://twitter.com/OLHSIndia`} target={"_blank"}><i className="bi bi-twitter"></i></Link></li>
+                              <li className={`px-2`}><Link href={`https://www.youtube.com/@OLHS`} target={"_blank"}><i className="bi bi-youtube"></i></Link></li>
                           </ul>
                       </div>
 

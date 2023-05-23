c5c2d3cf485b7021dfd13ea28ee2836cafe024d6
shashi <shashi09568@gmail.com>
2023-05-23 15:20:06 +0530

    changes

--- components/nav/MainNav.tsx
+++ components/nav/MainNav.tsx
@@ -174,77 +174,87 @@ export default function MainNav({cartItems, menu}: any) {
                         </div>
                     </div>
 
-                        <div id="accordion">
-                            <div className="card">
-                                <div className="card-header" id="headingOne">
-                                    <h5 className="mb-0">
-                                        <button className="btn btn-link" data-toggle="collapse"
-                                                data-target="#collapseOne" aria-expanded="true"
-                                                aria-controls="collapseOne">
-                                            Collapsible Group Item #1
-                                        </button>
-                                    </h5>
-                                </div>
+                        <div className="accordion" id="accordionExample">
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapseOne" aria-expanded="true"
+                                            aria-controls="collapseOne">
+                                        PRODUCTS
+                                    </button>
+                                </h2>
+                                <div id="collapseOne" className="accordion-collapse collapse show"
+                                     data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
 
-                                <div id="collapseOne" className="collapse show" aria-labelledby="headingOne"
-                                     data-parent="#accordion">
-                                    <div className="card-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                                        cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                                        Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-                                        you probably haven't heard of them accusamus labore sustainable VHS.
+
+                                        <p>
+                                            <a data-bs-toggle="collapse"
+                                               href="#collapseExample" role="button" aria-expanded="false"
+                                               aria-controls="collapseExample">
+                                                WET AMENITIES
+                                            </a>
+
+                                        </p>
+                                        <div className="collapse" id="collapseExample">
+                                            <div className="card card-body">
+                                              <ul>
+                                                  <li><Link href={``}>Shampoo</Link></li>
+                                                  <li><Link href={``}>Shampoo</Link></li>
+                                                  <li><Link href={``}>Shampoo</Link></li>
+                                                  <li><Link href={``}>Shampoo</Link></li>
+                                                  <li><Link href={``}>Shampoo</Link></li>
+                                                  <li><Link href={``}>Shampoo</Link></li>
+                                                  <li><Link href={``}>Shampoo</Link></li>
+
+                                              </ul>
+                                            </div>
+                                        </div>
+
+
+
                                     </div>
                                 </div>
                             </div>
-                            <div className="card">
-                                <div className="card-header" id="headingTwo">
-                                    <h5 className="mb-0">
-                                        <button className="btn btn-link collapsed" data-toggle="collapse"
-                                                data-target="#collapseTwo" aria-expanded="false"
-                                                aria-controls="collapseTwo">
-                                            Collapsible Group Item #2
-                                        </button>
-                                    </h5>
-                                </div>
-                                <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo"
-                                     data-parent="#accordion">
-                                    <div className="card-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                                        cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                                        Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-                                        you probably haven't heard of them accusamus labore sustainable VHS.
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button"
+                                            data-bs-toggle="collapse" data-bs-target="#collapseTwo"
+                                            aria-expanded="false" aria-controls="collapseTwo">
+                                        Accordion Item #2
+                                    </button>
+                                </h2>
+                                <div id="collapseTwo" className="accordion-collapse collapse"
+                                     data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <strong>This is the second item's accordion body.</strong> It is hidden by
+                                        default, until the collapse plugin adds the appropriate classes that we use to
+                                        style each element. These classes control the overall appearance, as well as the
+                                        showing and hiding via CSS transitions. You can modify any of this with custom
+                                        CSS or overriding our default variables. It's also worth noting that just about
+                                        any HTML can go within the <code>.accordion-body</code>, though the transition
+                                        does limit overflow.
                                     </div>
                                 </div>
                             </div>
-                            <div className="card">
-                                <div className="card-header" id="headingThree">
-                                    <h5 className="mb-0">
-                                        <button className="btn btn-link collapsed" data-toggle="collapse"
-                                                data-target="#collapseThree" aria-expanded="false"
-                                                aria-controls="collapseThree">
-                                            Collapsible Group Item #3
-                                        </button>
-                                    </h5>
-                                </div>
-                                <div id="collapseThree" className="collapse" aria-labelledby="headingThree"
-                                     data-parent="#accordion">
-                                    <div className="card-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                                        cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                                        Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-                                        you probably haven't heard of them accusamus labore sustainable VHS.
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button"
+                                            data-bs-toggle="collapse" data-bs-target="#collapseThree"
+                                            aria-expanded="false" aria-controls="collapseThree">
+                                        Accordion Item #3
+                                    </button>
+                                </h2>
+                                <div id="collapseThree" className="accordion-collapse collapse"
+                                     data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <strong>This is the third item's accordion body.</strong> It is hidden by
+                                        default, until the collapse plugin adds the appropriate classes that we use to
+                                        style each element. These classes control the overall appearance, as well as the
+                                        showing and hiding via CSS transitions. You can modify any of this with custom
+                                        CSS or overriding our default variables. It's also worth noting that just about
+                                        any HTML can go within the <code>.accordion-body</code>, though the transition
+                                        does limit overflow.
                                     </div>
                                 </div>
                             </div>

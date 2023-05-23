9403124f87ce193795e7a85e31962ac2c814df0e
shashi <shashi09568@gmail.com>
2023-05-23 15:25:03 +0530

    changes

--- components/nav/MainNav.tsx
+++ components/nav/MainNav.tsx
@@ -227,13 +227,7 @@ export default function MainNav({cartItems, menu}: any) {
                                 <div id="collapseTwo" className="accordion-collapse collapse"
                                      data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <strong>This is the second item's accordion body.</strong> It is hidden by
-                                        default, until the collapse plugin adds the appropriate classes that we use to
-                                        style each element. These classes control the overall appearance, as well as the
-                                        showing and hiding via CSS transitions. You can modify any of this with custom
-                                        CSS or overriding our default variables. It's also worth noting that just about
-                                        any HTML can go within the <code>.accordion-body</code>, though the transition
-                                        does limit overflow.
+
                                     </div>
                                 </div>
                             </div>
@@ -242,19 +236,13 @@ export default function MainNav({cartItems, menu}: any) {
                                     <button className="accordion-button collapsed" type="button"
                                             data-bs-toggle="collapse" data-bs-target="#collapseThree"
                                             aria-expanded="false" aria-controls="collapseThree">
-                                        Accordion Item #3
+
                                     </button>
                                 </h2>
                                 <div id="collapseThree" className="accordion-collapse collapse"
                                      data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <strong>This is the third item's accordion body.</strong> It is hidden by
-                                        default, until the collapse plugin adds the appropriate classes that we use to
-                                        style each element. These classes control the overall appearance, as well as the
-                                        showing and hiding via CSS transitions. You can modify any of this with custom
-                                        CSS or overriding our default variables. It's also worth noting that just about
-                                        any HTML can go within the <code>.accordion-body</code>, though the transition
-                                        does limit overflow.
+
                                     </div>
                                 </div>
                             </div>
